b1ec38bed062af40123f697ff3a6a82334ece7a9
nawin <nawinbrokentooth@gmail.com>
2025-05-27 21:01:13 +0700

    fix: แก้ไขไฟล์database.js เพิ่มstrictQuery

--- chat-backend/config/database.js
+++ chat-backend/config/database.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const MONGO_URI = "mongodb+srv://Na_1122:Na112211Ok@cluster0.9ylcsxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // เปลี่ยนเป็น URI ของคุณถ้าใช้ MongoDB Atlas
 
+mongoose.set('strictQuery', false);
+
 // เชื่อมต่อกับ MongoDB
 mongoose.connect(MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
